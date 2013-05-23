86fc8e3346d780c8b3d493671d035e442e2bc3e1
Julien Bouquillon <julien@bouquillon.com>
2013-05-23 15:19:03 +0200

    refactor: multiple fixes:
     - add snap-options attribute to pass arbitrary snap.js options on instantiation
     - add snapper.toggle method at runtime
     - publish snapper in the scope to enable method calls from ng-click
     - remove child ng-transclude for snapContent directive

--- angular-snap.js
+++ angular-snap.js
@@ -1,30 +1,42 @@
 angular.module('snap', []);
 
 angular.module('snap')
-  .directive('snapContent', function () {
+  .directive('snapContent', [function () {
     'use strict';
     return {
       template: [
-        '<div class="ngSnap ngSnap-content">',
-          '<div ng-transclude></div>',
+        '<div class="ngSnap ngSnap-content" ng-transclude>',
         '</div>'
       ].join(''),
       restrict: 'A',
       transclude: true,
-      link: function postLink(scope, element, attrs) {
+      link: function postLink(scope, iElement, iAttrs) {
 
         // -----------------------------------------------------
         // If we have jQuery find the shelves and set the max and min positions
         // accordingly
         // -----------------------------------------------------
+        var snapOptions = {
+          element: iElement[0]
+        };
+        // override snap options if some provided in snap-options attribute
+        if (angular.isDefined(iAttrs.snapOptions)) {
+          angular.extend(snapOptions, angular.fromJson(iAttrs.snapOptions));
+        }
+        var snapper = new Snap(snapOptions);
 
-        var el = element[0]
-          , snapper = new Snap({
-            element: el
-          });
+        // add toggle method
+        snapper.toggle = function(target) {
+          var method = (snapper.state().state === target)?'close':'open';
+          snapper[method](target);
+        };
+
+        // publish to the scope so we have all builtin methods available in ng-click
+        scope.snapper = snapper;
+
       }
     };
-  });
+  }]);
 
 angular.module('snap')
   .directive('snapShelf', function () {
--- src/scripts/directives/snap-content.js
+++ src/scripts/directives/snap-content.js
@@ -1,27 +1,36 @@
 /*global angular, Snap */
 
 angular.module('snap')
-  .directive('snapContent', function () {
+  .directive('snapContent', [function () {
     'use strict';
     return {
-      template: [
-        '<div class="ngSnap ngSnap-content">',
-          '<div ng-transclude></div>',
-        '</div>'
-      ].join(''),
+      template: '<div class="ngSnap ngSnap-content" ng-transclude></div>',
       restrict: 'A',
       transclude: true,
-      link: function postLink(scope, element, attrs) {
+      link: function postLink(scope, iElement, iAttrs) {
 
         // -----------------------------------------------------
         // If we have jQuery find the shelves and set the max and min positions
         // accordingly
         // -----------------------------------------------------
+        var snapOptions = {
+          element: iElement[0]
+        };
+        // override snap options if some provided in snap-options attribute
+        if (angular.isDefined(iAttrs.snapOptions)) {
+          angular.extend(snapOptions, angular.fromJson(iAttrs.snapOptions));
+        }
+        var snapper = new Snap(snapOptions);
 
-        var el = element[0]
-          , snapper = new Snap({
-            element: el
-          });
+        // add toggle method
+        snapper.toggle = function(target) {
+          var method = (snapper.state().state === target)?'close':'open';
+          snapper[method](target);
+        };
+
+        // publish to the scope so we have all builtin methods available in ng-click
+        scope.snapper = snapper;
+
       }
     };
-  });
+  }]);
