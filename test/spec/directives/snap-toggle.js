7cbf720575724eccf2c7768611ff287a658c2c22
Justin <jus.russell@gmail.com>
2013-06-06 00:03:39 -0400

    fix: Don't use snap-content directive if it's not needed

--- test/spec/directives/snap-toggle.js
+++ test/spec/directives/snap-toggle.js
@@ -6,41 +6,31 @@
 describe('Directive: snapToggle', function() {
   beforeEach(module('snap'));
 
-  var button = [
-      '<div snap-content><button snap-toggle>',
-        'awesome content',
-      '</button></div>'
-      ].join('');
+  var button = '<button snap-toggle>foo</button>'
+    , rightButton = '<button snap-toggle="right">foo</button>'
+    , scope;
 
-  var rightButton = [
-      '<div snap-content><button snap-toggle="right">',
-        'awesome content',
-      '</button></div>'
-      ].join('');
-
-  var element;
+  beforeEach(inject(function($rootScope) {
+    scope = $rootScope.$new();
+    scope.snapper = {
+      toggle: angular.noop
+    };
+    spyOn(scope.snapper, 'toggle');
+  }));
 
   describe('behaviour', function() {
-    it('should call snapper toggle method on the left snap', inject(function($rootScope, $compile) {
-      element = angular.element(button);
-      element = $compile(element)($rootScope);
-
-      spyOn($rootScope.snapper, 'toggle');
-
-      element.find('button')[0].click();
-      expect($rootScope.snapper.toggle).toHaveBeenCalledWith('left');
-
+    it('should call snapper toggle method on the left snap', inject(function($compile) {
+      var element = angular.element(button);
+      element = $compile(element)(scope);
+      element[0].click();
+      expect(scope.snapper.toggle).toHaveBeenCalledWith('left');
     }));
 
     it('should call snapper toggle method on the right snap', inject(function($rootScope, $compile) {
-      element = angular.element(rightButton);
-      element = $compile(element)($rootScope);
-
-      spyOn($rootScope.snapper, 'toggle');
-      
-      element.find('button')[0].click();
-      expect($rootScope.snapper.toggle).toHaveBeenCalledWith('right');
-
+      var element = angular.element(rightButton);
+      element = $compile(element)(scope);
+      element[0].click();
+      expect(scope.snapper.toggle).toHaveBeenCalledWith('right');
     }));
   });
 });
