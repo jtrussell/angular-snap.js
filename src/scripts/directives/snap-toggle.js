c27917c8b4df70006fcd25d5d6caae2864d50e63
Justin <jus.russell@gmail.com>
2013-06-06 00:01:59 -0400

    style: Make required attribute status explicit
    We'll let the user decide if they want to use a button or  tag

--- src/scripts/directives/snap-toggle.js
+++ src/scripts/directives/snap-toggle.js
@@ -1,15 +1,18 @@
 angular.module('snap')
   .directive('snapToggle', function() {
       'use strict';
-      return function (scope, element, attrs) {
-        element.bind('click', function() {
-            if (scope.snapper !== undefined) {
-              if (attrs.snapToggle) {
-                scope.snapper.toggle(attrs.snapToggle);
-              } else {
-                scope.snapper.toggle('left');
+      return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+          element.bind('click', function() {
+              if (scope.snapper !== undefined) {
+                if (attrs.snapToggle) {
+                  scope.snapper.toggle(attrs.snapToggle);
+                } else {
+                  scope.snapper.toggle('left');
+                }
               }
-            }
-        });
+          });
+        }
       };
   });
