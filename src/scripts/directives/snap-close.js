ca229b5c387eb040c325258671a6ef38f1c96f7e
Justin Russell <jus.russell@gmail.com>
2013-06-13 20:54:03 -0400

    style: Match other directive style

--- src/scripts/directives/snap-close.js
+++ src/scripts/directives/snap-close.js
@@ -1,11 +1,15 @@
 angular.module('snap')
   .directive('snapClose', function() {
     'use strict';
-    return function (scope, element, attrs) {
-      element.bind('click', function() {
-        if (scope.snapper !== undefined) {
-          scope.snapper.close();
-        }
-      });
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        element.bind('click', function() {
+          if (scope.snapper !== undefined) {
+            scope.snapper.close();
+          }
+        });
+      }
     };
+    
   });
