dc0f0f9cfb556fd6315a67ea59878adcfe4fea12
Justin <jus.russell@gmail.com>
2013-06-13 22:00:59 -0400

    feature: Provide convenience methods for toggle/open/close

--- src/scripts/services/snap-snapper.js
+++ src/scripts/services/snap-snapper.js
@@ -1,18 +1,32 @@
 angular.module('snap')
-  .factory('snapSnapper', ['$q', function($q) {
-    var deferred = $q.defer()
-      , snapper = deferred.promise;
+  .factory('snapSnapper', [function() {
+    var snapper = null;
+
+    // Provide direct access to the snapper object and a few convenience methods
+    // for our directives.
 
     return {
+      // Used internally. If you're creating your own snapper object why are you
+      // bothering with angular-snap?
+      _set: function(s) {
+        snapper = s;
+      },
+
+      // Returns null until our `snap-content` initializes
       get: function() {
         return snapper;
       },
-      set: function(s) {
-        snapper = s;
-        if(deferred ) {
-          deferred.resolve(s);
-          deferred = null;
-        }
+
+      toggle: function(side) {
+        // code...
+      },
+
+      open: function(side) {
+        // code...
+      },
+
+      close: function() {
+        // code...
       }
     };
   }]);
