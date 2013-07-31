90ad8fae100495c5a37c3b0fa2b527e888c0b023
Justin Russell <jus.russell@gmail.com>
2013-07-30 20:52:04 -0400

    feature: Start building support for multiple snap instances

--- src/scripts/services/snap-remote.js
+++ src/scripts/services/snap-remote.js
@@ -2,42 +2,66 @@ angular.module('snap')
   .factory('snapRemote', ['$q', function($q) {
     'use strict';
 
-    // Provide direct access to the snapper object and a few convenience methods
-    // for our directives.
+    var snapperStore = {}
+      , DEFAULT_SNAPPER_ID = '__DEFAULT_SNAPPER_ID__'
+      , exports = {}
+      , initStoreForId
+      , resolveInStoreById;
 
-    var deferred = $q.defer()
-      , exports;
-  
-    exports = {
-      get: function() {
-        return deferred.promise;
-      },
-
-      register: function(snapper) {
-        deferred.resolve(snapper);
-      },
-
-      toggle: function(side) {
-        exports.get().then(function(snapper) {
-          if(side === snapper.state().state) {
-            exports.close(side);
-          } else {
-            exports.open(side);
-          }
-        });
-      },
-
-      open: function(side) {
-        exports.get().then(function(snapper) {
-          snapper.open(side);
-        });
-      },
-
-      close: function() {
-        exports.get().then(function(snapper) {
-          snapper.close();
-        });
+    exports.get = function(id) {
+      id = id || DEFAULT_SNAPPER_ID;
+      if(!snapperStore.hasOwnProperty(id)) {
+        initStoreForId(id);
       }
+      return snapperStore[id].deferred.promise;
+    };
+
+    exports.register = function(snapper, id) {
+      id = id || DEFAULT_SNAPPER_ID;
+      if(!snapperStore.hasOwnProperty(id)) {
+        initStoreForId(id);
+      }
+      if(snapperStore[id].isResolved) {
+        initStoreForId(id);
+      }
+      resolveInStoreById(snapper, id);
+    };
+
+    exports.toggle = function(side, id) {
+      id = id || DEFAULT_SNAPPER_ID;
+      exports.get().then(function(snapper) {
+        if(side === snapper.state().state) {
+          exports.close(side);
+        } else {
+          exports.open(side);
+        }
+      });
+    };
+
+    exports.open = function(side, id) {
+      id = id || DEFAULT_SNAPPER_ID;
+      exports.get().then(function(snapper) {
+        snapper.open(side);
+      });
+    };
+
+    exports.close = function(id) {
+      id = id || DEFAULT_SNAPPER_ID;
+      exports.get().then(function(snapper) {
+        snapper.close();
+      });
+    };
+
+    initStoreForId = function(id) {
+      snapperStore[id] = {
+        deferred: $q.defer(),
+        isResolved: false
+      };
+    };
+
+    resolveInStoreById = function(snapper, id) {
+      snapperStore[id].deferred.resolve(snapper);
+      snapperStore[id].isResolved = true;
     };
 
     return exports;
