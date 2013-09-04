7c7af942f26df069050410901621479327c072c3
Jonathan Sanchez <jsanchezpando@cirb.irisnet.be>
2013-09-04 09:58:10 +0200

    remove "replace" and "translude" instructions

--- src/scripts/directives/snap-drawers.js
+++ src/scripts/directives/snap-drawers.js
@@ -2,9 +2,10 @@ angular.module('snap')
   .directive('snapDrawers', function () {
     'use strict';
     return {
-      transclude: true,
-      replace: true,
       restrict: 'AE',
-      template: '<div class="snap-drawers" ng-transclude></div>'
+      link: function(scope, element, attrs) {
+        element.addClass('snap-drawers');
+      }
     };
   });
+
