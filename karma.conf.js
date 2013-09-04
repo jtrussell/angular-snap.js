b147f88b228cc788f74178f0bbbcdd1aa57c311f
Justin <jrussell@ivantagehealth.com>
2013-09-04 08:10:27 -0400

    chore: Update karma to latest version

--- karma.conf.js
+++ karma.conf.js
@@ -1,57 +1,24 @@
-// Karma configuration
+module.exports = function(config) {
+  config.set({
+    autoWatch: true,
+    singleRun: false,
+    frameworks: ['jasmine'],
+    files: [
+      'test/components/angular/angular.js',
+      'test/components/angular-mocks/angular-mocks.js',
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+      'src/scripts/*.js',
+      'src/scripts/**/*.js',
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'test/components/angular/angular.js',
-  'test/components/angular-mocks/angular-mocks.js',
-
-  'src/scripts/*.js',
-  'src/scripts/**/*.js',
-
-  'test/spec/**/*.js'
-];
-
-// list of files to exclude
-exclude = [];
-
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
-
-// web server port
-port = 8080;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+      'test/spec/**/*.js'
+    ],
+    browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
+    reporters: ['progress'],
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher'
+    ],
+    logLevel: config.LOG_INFO
+  });
+};
