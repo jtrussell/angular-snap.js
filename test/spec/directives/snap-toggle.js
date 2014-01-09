dc3f7555044dabbeba54e844f2f5bcfc98fadceb
Justin <jus.russell@gmail.com>
2014-01-09 08:05:19 -0500

    test: Remove some cruft
    Was injecting dependencies we don't need...

--- test/spec/directives/snap-toggle.js
+++ test/spec/directives/snap-toggle.js
@@ -20,20 +20,20 @@ describe('Directive: snapToggle', function() {
   }));
 
   describe('behaviour', function() {
-    it('should call snapper toggle method on the left snap', inject(function($compile) {
+    it('should call snapper toggle method on the left snap', function() {
       var element = angular.element(button);
       element = compile(element)(scope);
       element[0].click();
       expect(snapRemote.toggle.mostRecentCall.args[0]).toEqual('left');
       expect(snapRemote.toggle.mostRecentCall.args[1]).toBeUndefined();
-    }));
+    });
 
-    it('should call snapper toggle method on the right snap', inject(function($rootScope, $compile) {
+    it('should call snapper toggle method on the right snap', function() {
       var element = angular.element(rightButton);
       element = compile(element)(scope);
       element[0].click();
       expect(snapRemote.toggle.mostRecentCall.args[0]).toEqual('right');
       expect(snapRemote.toggle.mostRecentCall.args[1]).toEqual('mySnapId');
-    }));
+    });
   });
 });
