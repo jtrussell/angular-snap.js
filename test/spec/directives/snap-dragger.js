1b94b03cbd107be27acdeaa810eca9920de18cec
Justin Russell <jus.russell@gmail.com>
2014-01-09 22:17:45 -0500

    test: Finish basic test for dragger directive
    Skipping build (will fail as there as of yet no dragger directive)
    
    [ci skip]

--- test/spec/directives/snap-dragger.js
+++ test/spec/directives/snap-dragger.js
@@ -13,18 +13,33 @@ ddescribe('Directive: snapDragger', function() {
     ].join('')
     , snapRemote
     , compile
-    , scope;
+    , scope
+    , snapperDummy
+    , SnapSpy;
 
   beforeEach(inject(function($rootScope, $compile) {
     scope = $rootScope.$new();
     compile = $compile;
   }));
 
+  beforeEach(function() {
+    snapperDummy = {
+      settings: jasmine.createSpy('settings')
+    };
+
+    SnapSpy = jasmine.createSpy('Snap').andReturn(snapperDummy);
+    window.Snap = SnapSpy;
+  });
+
   describe('behaviour', function() {
-    it('should pass through our dragger to the Snap.js contstructor', function() {
+    it('should update the snapper settings to use the dragger', function() {
       var element = angular.element(tpl);
       element = compile(element)(scope);
       scope.$apply();
+
+      expect(snapperDummy.settings).toHaveBeenCalledWith({
+        dragger: element.find('div')[0]
+      });
     });
 
   });
