4d1a8edeb8b955227802da245deab221ff5323cc
Justin Russell <jus.russell@gmail.com>
2014-07-03 22:01:25 -0400

    fix: Bad write from c9?

--- src/scripts/constants/snap-version.js
+++ src/scripts/constants/snap-version.js
@@ -2,11 +2,12 @@
 
 (function() {
   'use strict';
-  var version = [1, 5, 2]
+  var version = [1, 5, 3]
     , vObj = {
         full: version.join('.'),
         major: version[0],
         minor: version[1],
         patch: version[2]
       };
-  angular.module('snap').constant('SNAP_VERSIO
+  angular.module('snap').constant('SNAP_VERSION', vObj);
+}());
