b1af9f8845f35669c41fed184a0f116c83fa5bd0
Justin Russell <jus.russell@gmail.com>
2014-12-28 19:24:52 -0500

    test: Fix typo... jasmin is not defined

--- test/spec/directives/snap-content.js
+++ test/spec/directives/snap-content.js
@@ -158,7 +158,7 @@ describe('Directive: snapContent', function() {
       it('should update the snapper when an option changes', function() {
         scope.foobar = 23;
         scope.$apply();
-        expect(snapperDummy.settings).toHaveBeenCalledWith(jasmin.objectContaining({
+        expect(snapperDummy.settings).toHaveBeenCalledWith(jasmine.objectContaining({
           foobar: 23
         }));
       });
